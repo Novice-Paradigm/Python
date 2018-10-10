15cc1f6475907bc981a14e273c693431ef825cda
Nishant Charas <kingpsy11@gmail.com>
2018-10-10 18:54:02 +0530

    Hactoberfest PR
    added an additional algorithm for finding factorial.

---
+++ Src/factorial/Using_math_library.py
@@ -0,0 +1,10 @@
+"""
+Created on Wed Oct 8 18:36:59 2018
+
+@author: nisnt2411
+"""
+import math 
+print ("The factorial of 2411 is : ", end="") 
+print (math.factorial(2411)) 
+# we can use python's inbuilt function .factorial
+# It throws a runtime error when a negative number is passed to it as factorial of negative numbers is not defined.
