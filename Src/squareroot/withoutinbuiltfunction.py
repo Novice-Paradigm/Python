51e5f60f474f44b0ed30450b4751668ce7e74923
acollie <12976663+Acollie@users.noreply.github.com>
2018-10-13 14:48:34 +0100

    Adding two functions requested

---
+++ Src/squareroot/withoutinbuiltfunction.py
@@ -0,0 +1,9 @@
+# This square roots n by raising the number to the power of 0.5
+def squareroot(n):
+
+    return n**0.5
+
+n = int(input("Enter number whose squareroot is wanted "))
+print " %d = %.2f " %(n,squareroot(n))
+# the %.2f means the decimal accuracy
+# This code does not work for negative numbers for obvious reasons.
