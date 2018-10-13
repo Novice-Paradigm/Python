8f7fb5d3a20d554fbc543dcc14d4c0b203ae7230
Kai Chang <kaijchang@gmail.com>
2018-10-13 08:51:08 -0700

    kilometers to miles

---
+++ Src/kilometerstomiles.py
@@ -0,0 +1,11 @@
+def kilometers_to_miles(kilometers):
+    return 0.621371 * kilometers
+
+
+def miles_to_kilometers(miles):
+    return 1.60934 * miles
+
+
+if __name__ == "__main__":
+    print("5 kilometers to miles: {}".format(kilometers_to_miles(5)))
+    print("5 miles to kilometers: {}".format(miles_to_kilometers(5)))
