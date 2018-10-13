6fcb8baca2846d153df18a885c1110029c2476b4
Hung Pham <hungphamquoc2202@gmail.com>
2018-10-14 03:42:34 +0800

    HungPham/Check number

---
+++ Src/check_num.py
@@ -0,0 +1,8 @@
+# Check whether a number is +ve, -ve or 0 #60
+a = int(input("Input a number: "))
+if(a < 0):
+	print("a is negative")
+elif(a > 0):
+	print("a is positive")
+else:
+	print("a is equql to 0")
