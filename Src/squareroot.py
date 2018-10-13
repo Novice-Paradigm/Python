fa300bf69511ae4c08cfec57a4068bd087d0827c
goore3 <38388042+goore3@users.noreply.github.com>
2018-10-13 18:43:59 +0200

    Delete squareroot

--- Src/squareroot.py
+++
@@ -1,9 +0,0 @@
-from math import sqrt
-def square_root_inbuilt(x):
-    return sqrt(x)
-def square_root_manual(x):
-    margin = 0.00001
-    y = float(x)
-    while ((y - x / y) > margin):
-        y = (y + x / y) / 2
-    return round(y,5)
