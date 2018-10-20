7dcfef9e98ab1f05b76706e6e2ce5cd2b478704e
Anuj Kumar Singh <37617602+devilitself@users.noreply.github.com>
2018-10-21 00:55:16 +0530

    Described Input and Output format also for Palindrome Checker

--- Src/PalindromeChecker.py
+++ Src/PalindromeChecker.py
@@ -1,3 +1,5 @@
+# Input : A String is given as input
+# Output : Print " Given String is Palindrome " without the double quotes if Input String is Palindrome otherwise print " Given String is not Palindrome "
 s=input()
 if s==s[::-1] :
 	print("Given String is Palindrome")
