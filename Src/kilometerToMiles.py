03bc6962af0ddb50e2b65182d1002240eec2d45d
Jugal Rawlani <rawlani.98@gmail.com>
2018-10-27 06:55:39 +0530

    Add kilometer to mile program fixed issue #27

---
+++ Src/kilometerToMiles.py
@@ -0,0 +1,7 @@
+
+kilometer = input('Enter Distance in kilometer : ')
+kilometer = float(kilometer)
+nautical_miles = kilometer / 1.852
+land_miles = kilometer / 1.609
+print('Temperature in Nautical Miles :', nautical_miles)
+print('Temperature in Land Miles :', land_miles)
