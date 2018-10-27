697215abc918e75af5abdd06f3968af1a9161f81
mzeeqazi <mzeeqazi@outlook.com>
2018-10-27 22:20:00 +0530

    bayes.py ported to Python 3.x

--- Src/bayes.py
+++ Src/bayes.py
@@ -1,6 +1,6 @@
 #Implementation of bayes theorem for two events which depend on a third event
-pmb = input()
-pab = input()
-p1 =  input()
+pmb = float(input())
+pab = float(input())
+p1 =  float(input())
 x = p1*(pab*(1-pmb)+ pmb*(1-pab))
-print '%.6f' %x,
+print ('%.6f' %x,)
