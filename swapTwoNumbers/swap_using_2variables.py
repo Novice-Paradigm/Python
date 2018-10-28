afa99d14f4dd54d4683d1b94eaa49058f58ae181
abhiishekporwal <37709497+abhiishekporwal@users.noreply.github.com>
2018-10-29 03:55:01 +0530

    Add files via upload

---
+++ swapTwoNumbers/swap_using_2variables.py
@@ -0,0 +1,9 @@
+
+a = 5     # for the manual integer input we use a= int(input())
+b = 9	  # similary, for b=int(input())
+   
+a = a + b   # a is now 14
+b = a - b   # b becomes 5 
+a = a - b   # a becomes 9 
+
+print("After Swapping: a =",a ," b =", b);
