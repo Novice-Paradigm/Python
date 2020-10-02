e14179646448b780391d0d9c6164d6c221a25839
adityasingh22 <31698903+adityasingh22@users.noreply.github.com>
2020-10-02 13:53:31 +0530

    Update Check whether a string is Palindrome or not.py

--- Algorithms/Check whether a string is Palindrome or not.py
+++ Algorithms/Check whether a string is Palindrome or not.py
@@ -1,10 +1,8 @@
 # Palindrome is a string which written backwards is the same as the string. We use reverse method and then compare the strings to see if they are palindrome.
 
-'''python
 string=input()
 stringr=string[::-1]
 if string==stringr:
   print("Palindrome")
 else:
   print("Not Palindrome")
-'''
